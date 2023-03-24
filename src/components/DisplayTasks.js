79875761e21d632979f5eaf6e557879207a8ef29
shawazi <shawazisonfire@gmail.com>
2023-03-24 02:32:09 -0400

    distributed subtask status into columns according to boolean, still need to implement dragging and switch booleans to opposite value on drag

--- src/components/DisplayTasks.js
+++ src/components/DisplayTasks.js
@@ -5,36 +5,57 @@ const DisplayTasks = ({ file }) => {
 
   useEffect(() => {
     if (file) {
-        setTasks(JSON.parse(file));
-        // console.log(tasks);
+      setTasks(JSON.parse(file));
+      // console.log(tasks);
     }
   }, [file]);
-    
+
   return (
     <div>
-        {file && Object.entries(tasks).map((task) => (
-            console.log(task)
-        ))}
-      {/* {file && Object.entries(tasks).map((task, index) => (
-        <div key={index}>
-          <h3>{task}</h3>
-          <table className="table">
-            <thead className="table-dark">
-              <tr>
-                <th scope="col">Incomplete</th>
-                <th scope="col">Complete</th>
-              </tr>
-            </thead> */}
-            {/* <tbody>
-              {Object.keys(tasks[task]).map((subtask, subtaskIndex) => (
-                <tr key={subtaskIndex}>
-                  <td>{tasks[task][subtask]}</td>
+      {file && Object.entries(tasks).map((task) => console.log(task))}
+      {file &&
+        Object.entries(tasks).map(([taskName, taskData], index) => (
+          <div
+            className="border border-dark d-flex flex-column gap-2"
+            key={index}
+          >
+            <h3>{taskName}</h3>
+            <table className="table">
+              <thead className="table-dark">
+                <tr>
+                  <th className="w-75" scope="col">
+                    Subtask
+                  </th>
+                  <th scope="col">Incomplete</th>
+                  <th scope="col">Complete</th>
                 </tr>
-              ))}
-            </tbody> */}
-          {/* </table>
-        </div>
-      ))} */}
+              </thead>
+              <tbody>
+                {Object.entries(tasks[taskName]).map(
+                  ([subtask, status], subtaskIndex) => {
+                    const [subtaskName, subtaskStatus] =
+                      Object.entries(status)[0];
+                    return (
+                      <tr key={subtaskIndex}>
+                        <td draggable>{subtaskName}</td>
+                        {subtaskStatus ? (
+                          <td draggable>{JSON.stringify(subtaskStatus)}</td>
+                        ) : (
+                          <td draggable></td>
+                        )}
+                        {!subtaskStatus ? (
+                          <td draggable>{JSON.stringify(subtaskStatus)}</td>
+                        ) : (
+                          <td draggable></td>
+                        )}
+                      </tr>
+                    );
+                  }
+                )}
+              </tbody>
+            </table>
+          </div>
+        ))}
     </div>
   );
 };
